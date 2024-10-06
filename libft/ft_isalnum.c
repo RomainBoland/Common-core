558d84cb159c50b658fbd344091978c2034327ec
RomainBoland <romain.boland@hotmail.com>
2024-10-06 17:12:05 +0200

    Initial commit

---
+++ libft/ft_isalnum.c
@@ -0,0 +1,15 @@
+int	ft_isalnum(int c)
+{
+	if ((c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9') || (c >= 'a' && c <= 'z'))
+		return (1);
+	else
+		return (0);
+}
+/*
+#include <stdio.h>
+int	main(void)
+{
+	printf("%d\n", ft_isalnum(-1));
+	return (0);
+}
+*/
---
+++ libft/ft_isdigit.c
@@ -0,0 +1,16 @@
+int	ft_isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	printf("%d\n", ft_isdigit(-1));
+	return (0);
+}
+*/
