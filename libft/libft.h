d064944a70f0f426cb71b8236bd3828487aefdf1
RomainBoland <romain.boland@hotmail.com>
2024-10-10 17:27:34 +0200

    memmove / update makefile et .h

--- libft/libft.h
+++ libft/libft.h
@@ -13,5 +13,7 @@
 	int	ft_atoi(char *str);
 	void	*ft_memcpy(void *dst, const void *src, size_t size);
 	void	*ft_memset(void *pointer, int value, size_t len);
+	void	*ft_memchr(const void *s, int c, size_t n);
+	void    *ft_memmove(void *dst, const void *src, size_t len)
 
 #endif
