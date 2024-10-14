b49207c3df16f3889e884b85f8a5b02af42a813d
RomainBoland <romain.boland@hotmail.com>
2024-10-14 19:45:50 +0200

    ft_memcmp

--- libft/libft.h
+++ libft/libft.h
@@ -29,5 +29,7 @@ void	*ft_memcpy(void *dst, const void *src, size_t size);
 void	*ft_memset(void *pointer, int value, size_t len);
 void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+
 
 #endif
