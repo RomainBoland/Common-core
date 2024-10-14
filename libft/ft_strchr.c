0b72b28bf453cd634dc2776202863db99055a130
RomainBoland <romain.boland@hotmail.com>
2024-10-14 20:11:50 +0200

    ft_strchr

---
+++ libft/ft_strchr.c
@@ -0,0 +1,19 @@
+#include "libft.h"
+
+char *ft_strchr(const char *s, int c)
+{
+	int		i;
+	char	tmp_c;
+
+	tmp_c = (char) c;
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+			return ((char *)&s[i]);
+		i++;
+	}
+	if (s[i] == c)
+			return ((char *)&s[i]);
+	return (NULL);
+}
--- libft/libft.h
+++ libft/libft.h
@@ -30,6 +30,6 @@ void	*ft_memset(void *pointer, int value, size_t len);
 void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
-
+char 	*ft_strchr(const char *s, int c)
 
 #endif
