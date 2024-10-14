319b9eb2c255d1aa0805b94efa542cf61a25c8e0
RomainBoland <romain.boland@hotmail.com>
2024-10-14 19:31:02 +0200

    ft_memchr

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -3,5 +3,27 @@
 
 void *ft_memchr(const void *s, int c, size_t n)
 {
-	
+	unsigned char	tmp_c;
+	unsigned char	*tmp_s;
+	size_t			i;
+
+	i = 0;
+	tmp_c = (unsigned char) c;
+	tmp_s = (unsigned char *) s;
+	while (i < n)
+	{
+		if (tmp_s[i] == tmp_c)
+			return ((void *)&tmp_s[i]);
+		i++;
+	}
+	return (NULL);
 }
+/*
+#include <stdio.h>
+
+int main(void)
+{
+	printf("%p\n", ft_memchr("oui", 'u', 2));
+	return (0);
+}
+*/
