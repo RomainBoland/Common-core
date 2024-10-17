94e3bd46f7a46d32576a72eb23a49c0d8a692bb7
RomainBoland <romain.boland@hotmail.com>
2024-10-17 12:15:50 +0200

    ft_strnstr

---
+++ libft/ft_strnstr.c
@@ -0,0 +1,38 @@
+#include "libft.h"
+
+char *ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	i;
+    size_t	j;
+
+	i = 0;
+    if (!needle[i])
+    {
+        return ((char *)&haystack[i]);
+    }
+    while (haystack[i] && i < len)
+    {
+        j = 0;
+        while ((haystack[i + j]) && (haystack[i + j] == needle[j] && i + j < len))
+        {
+            if (!needle[j + 1])
+            {
+                return ((char *)&haystack[i]);
+            }
+            j++;
+        }
+        i++;
+    }
+    return (0);
+}
+/*
+#include <stdio.h>
+int     main(void)
+{
+        char    str[] = "Hello world";
+        char    to_find[] = "lo";
+
+        printf("%s\n", ft_strnstr(str, to_find, 12));
+        return (0);
+}
+*/
--- libft/libft.h
+++ libft/libft.h
@@ -36,5 +36,6 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 int	ft_strncmp(const char *s1, const char *s2, size_t n);
 void	ft_bzero(void *s, size_t n);
+char *ft_strnstr(const char *haystack, const char *needle, size_t len);
 
 #endif
