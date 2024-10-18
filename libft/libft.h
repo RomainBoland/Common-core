28c019241949a4574539bac556089f17f8361405
RomainBoland <romain.boland@hotmail.com>
2024-10-18 15:40:39 +0200

    ft_splitv3

--- libft/libft.h
+++ libft/libft.h
@@ -42,5 +42,6 @@ void	*ft_calloc(size_t count, size_t size);
 char	*ft_substr(const char *s, unsigned int start, size_t len);
 char	*ft_strjoin(const char *s1, const char *s2);
 char	*ft_strtrim(const char *s1, const char *set);
+char    **ft_split(char *str, char charset);
 
 #endif
