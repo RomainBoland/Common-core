a63e690b5ac85833f0ae90f3aaa98da3e3ff9fe0
root <root@jjiro>
2024-10-24 15:02:48 +0200

    last correction atoi

--- libft/libft.h
+++ libft/libft.h
@@ -31,7 +31,7 @@ int		ft_isprint(int c);
 size_t	ft_strlen(const char *str);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
-int		ft_atoi(char *str);
+int		ft_atoi(const char *str);
 void	*ft_memcpy(void *dst, const void *src, size_t size);
 void	*ft_memset(void *pointer, int value, size_t len);
 void	*ft_memchr(const void *s, int c, size_t n);
