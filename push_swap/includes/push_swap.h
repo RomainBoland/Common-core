d2efd19cf99e0c88592ceed1aed5bc88948cffd4
RomainBoland <romain.boland@hotmail.com>
2025-01-02 12:49:17 +0100

    Foldering Almost done

---
+++ push_swap/includes/push_swap.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rboland <rboland@student.s19.be>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/02 09:53:01 by rboland           #+#    #+#             */
+/*   Updated: 2025/01/02 09:53:01 by rboland          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include "../libft/includes/libft.h"
+
+typedef struct s_stack
+{
+	int				pos;
+	int				*content;
+	struct s_stack	*next;
+}	t_stack;
+
+#endif
